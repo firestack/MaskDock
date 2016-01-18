dbbdd3d0f0f40048d6cdf49bb9dab0fb34b4860e
Firestack <me@stackfire.io>
2016-01-18 00:09:17 -0700

    Testing

---
+++ Assets/setup.sh
@@ -0,0 +1,8 @@
+cd /srv
+git clone --depth=1 --branch server https://github.com/firestack/MaskOfTruth.git MaskofTruth
+
+cd ./MaskofTruth
+
+## pip install from file here
+
+pip3 install requests
---
+++ Assets/start.sh
@@ -0,0 +1,10 @@
+
+set -xe
+
+echo "Entering Mask Of Truth Directory"
+cd /srv/MaskofTruth
+
+echo "Starting The bot..."
+sleep 3s
+
+python3 NewBot.py
